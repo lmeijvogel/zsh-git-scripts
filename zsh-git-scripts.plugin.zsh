8e62970b6bac8b992412e1a1f78631ffa90734bb
Lennaert Meijvogel <lmeijvogel@yahoo.co.uk>
2020-04-25 10:36:28 +0200

    Rename shell files to have hyphens instead of underscores.

--- zsh-git-scripts.plugin.zsh
+++ zsh-git-scripts.plugin.zsh
@@ -1,10 +1,10 @@
 export ZSH_GIT_SCRIPTS_DIR=$(/bin/readlink -f ${0%/*})
 
-source $ZSH_GIT_SCRIPTS_DIR/__convert_indices.zsh
-source $ZSH_GIT_SCRIPTS_DIR/git_choose_branch.zsh
-source $ZSH_GIT_SCRIPTS_DIR/git_choose_remote_branch.zsh
-source $ZSH_GIT_SCRIPTS_DIR/expand_indices.zsh
+source $ZSH_GIT_SCRIPTS_DIR/__convert-indices.zsh
+source $ZSH_GIT_SCRIPTS_DIR/expand-indices.zsh
 source $ZSH_GIT_SCRIPTS_DIR/git-add.zsh
+source $ZSH_GIT_SCRIPTS_DIR/git-choose-branch.zsh
+source $ZSH_GIT_SCRIPTS_DIR/git-choose-remote-branch.zsh
 source $ZSH_GIT_SCRIPTS_DIR/git-diff.zsh
 source $ZSH_GIT_SCRIPTS_DIR/git-log.zsh
 source $ZSH_GIT_SCRIPTS_DIR/git-status.zsh
