8c8cc02873dd22d69a30b6cfb5c57f106a9252ac
Lennaert Meijvogel <lennaert.meijvogel@mendix.com>
2021-04-23 13:22:46 +0200

    Add script for git add with fzf.

---
+++ git-choose-files-from-status.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+
+# This calls `git add` with the selected file(s).
+#
+# It passes command line flags to `git add`, so calling this script with e.g.
+# `--patch` will perform a `git add --patch`, as expected.
+git status --porcelain \
+  | fzf -m --preview "git diff --color=always {-1}" \
+  | awk '// { print $2 }' \
+  | xargs -t -o git add $@
